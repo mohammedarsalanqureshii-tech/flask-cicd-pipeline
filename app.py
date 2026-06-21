05aad8c4efd75394a21cf4863acd4977936ba5f3
mohammedarsalanqureshii-tech <mohammedarsalanqureshii@gmail.com>
2026-06-21 09:21:49 +0000

    Add health check point

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import socket
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Version 2 deployed automatically by Github Actions!"
+    return jsonify({
+        "message": "Flask app running on Kubernetes",
+        "hostname": socket.gethostname()
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
